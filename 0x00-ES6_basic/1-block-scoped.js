524cfbca315e8ad5fc50233a678c64d5053c012e
mostsfa538 <mostafa51moktar@gmail.com>
2024-07-22 15:01:39 +0300

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,9 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    // no-unused-vars
+    // eslint-disable-next-line no-unused-vars
     const task = true;
-    // no-unused-vars
+    // eslint-disable-next-line no-unused-vars
     const task2 = false;
   }
 
