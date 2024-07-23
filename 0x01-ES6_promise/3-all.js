a581111804518d1b661e779fcc3a40e764bae279
mostsfa538 <mostafa51moktar@gmail.com>
2024-07-23 14:48:48 +0300

    3-all.js

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([uploadPhotoResult, createUserResult]) => {
+      console.log(`${uploadPhotoResult.body} ${createUserResult.firstName} ${createUserResult.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
