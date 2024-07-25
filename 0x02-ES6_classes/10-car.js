d9401b2cea63db197d6501651e682b052f44913e
mostsfa538 <mostafa51moktar@gmail.com>
2024-07-25 19:56:07 +0300

    10-car.js

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,20 @@
+const cloneSymbol = Symbol('clone');
+
+class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [cloneSymbol]() {
+    return this;
+  }
+
+  cloneCar() {
+    const ClonedCar = this.constructor[cloneSymbol];
+    return new ClonedCar();
+  }
+}
+
+export default Car;
