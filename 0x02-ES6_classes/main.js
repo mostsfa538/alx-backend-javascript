f04c1214aa06642e665ab451a2e9ab9eb6fd0604
mostsfa538 <mostafa51moktar@gmail.com>
2024-07-25 19:15:47 +0300

    4-pricing.js

--- 0x02-ES6_classes/main.js
+++ 0x02-ES6_classes/main.js
@@ -1,13 +1,6 @@
-import Building from './5-building.js';
+import Pricing from './4-pricing.js';
+import Currency from './3-currency.js';
 
-const b = new Building(100);
-console.log(b);
-
-class TestBuilding extends Building {}
-
-try {
-    new TestBuilding(200)
-}
-catch(err) {
-    console.log(err);
-}
+const p = new Pricing(100, new Currency("EUR", "Euro"))
+console.log(p);
+console.log(p.displayFullPrice());
