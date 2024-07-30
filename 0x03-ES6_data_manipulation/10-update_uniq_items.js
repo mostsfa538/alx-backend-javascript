ea7d183f1f6b0b42c1d8736ad9557b9544ba7480
mostsfa538 <mostafa51moktar@gmail.com>
2024-07-30 15:21:25 +0300

    10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -3,9 +3,9 @@ export default function updateUniqueItems(map) {
     throw new Error('Cannot process');
   }
 
-  for (let [key, value] of map) {
-    if (value === 1) {
-      map.set(key, 100);
+  for (const [item, quantity] of map) {
+    if (quantity === 1) {
+      map.set(item, 100);
     }
   }
 
