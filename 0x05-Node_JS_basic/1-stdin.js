92207664540cc59e67baa1fdcd28f53853991e16
mostsfa538 <mostafa51moktar@gmail.com>
2024-09-18 13:05:31 +0300

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,12 @@
 console.log('Welcome to Holberton School, what is your name?')
+
 process.stdin.on('data', data => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing')
+
+  if (!process.stdin.isTTY) {
+    console.log('This important software is now closing')
+  }
+
   process.exit();
 });
