38f5a3932836bf9cf4bbfbf18cae6b1fdc3d9e91
mostsfa538 <mostafa51moktar@gmail.com>
2024-09-18 12:58:31 +0300

    Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,6 @@
+console.log('Welcome to Holberton School, what is your name?')
+process.stdin.on('data', data => {
+  console.log(`Your name is: ${data.toString().trim()}`);
+  console.log('This important software is now closing')
+  process.exit();
+});
