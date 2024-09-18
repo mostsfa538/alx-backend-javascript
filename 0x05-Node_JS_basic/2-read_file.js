5ebf7d1ed2d14641bb425ed9de9a26aa379adb87
mostsfa538 <mostafa51moktar@gmail.com>
2024-09-18 14:15:19 +0300

    Reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+
+function countStudents(path) {
+  try {
+    data = fs.readFileSyn(path, 'utf-8');
+  } catch (error) {
+    console.log('Cannot load the database')
+    process.exit();
+  }
+  data = data.split('\n').filter((line) => line.trim() !== '');
+  data.shift();
+  const numberOfStd = data.length;
+  const fields = {};
+
+  for (const i in data) {
+    const line = data[i].split(',');
+    if (!fields[line[3]]) {
+      fields[line[3]] = [];
+    }
+    fields[line[3]].push(line[0]);
+  }
+  console.log(`Number of students: ${numberOfStd}`);
+  Object.keys(fields).forEach((field) => {
+    console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+  });
+}
+
+module.exports = countStudents;
