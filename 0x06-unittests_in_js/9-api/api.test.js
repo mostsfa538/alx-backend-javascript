ed7ab18e937f83e10c55196732c62ae5e0149eac
mostsfa538 <mostafa51moktar@gmail.com>
2024-09-20 20:20:20 +0300

    Regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,50 @@
+const request = require('request');
+const expect = require('chai').expect;
+
+describe('Index page', () => {
+    const url = 'http://localhost:7865';
+
+    it('responds with status code 200', async () => {
+        request(url, (err, res, body) => {
+            expect(res.statusCode).to.equal(200);
+        });
+    });
+
+    it('responds with correct result', async () => {
+        request(url, (err, res, body) => {
+            expect(body).to.equal('Welcome to the payment system');
+        });
+    });
+
+    it('responds with correct headers', async () => {
+        request(url, (err, res, body) => {
+            expect(res.headers['content-type']).to.equal('text/html; charset=utf-8');
+            expect(res.headers['content-length']).to.equal('29');
+        });
+    });
+
+    it('responds with status code 200', async () => {
+        request(url + '/cart/123', (err, res, body) => {
+            expect(res.statusCode).to.equal(200);
+        });
+    });
+
+    it('responds with status code 404', async () => {
+        request(url + '/cart/abs', (err, res, body) => {
+            expect(res.statusCode).to.equal(404);
+        });
+    });
+
+    it('responds with correct result', async () => {
+        request(url + '/cart/8', (err, res, body) => {
+            expect(body).to.equal(`Payment methods for cart 8`);
+        });
+    });
+
+    it('responds with correct headers', async () => {
+        request(url + '/cart/8', (err, res, body) => {
+            expect(res.headers['content-type']).to.equal('text/html; charset=utf-8');
+            expect(res.headers['content-length']).to.equal(`${'Payment methods for cart 8'.length}`);
+        });
+    });
+});
